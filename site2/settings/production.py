b3826c8efe6337f3170efbbc03136cdbff30dfb9
Matt Lavis <matthew.lavis@gmail.com>
2021-05-29 22:57:12 +0100

    Prep for prod

--- site2/settings/production.py
+++ site2/settings/production.py
@@ -1,4 +1,5 @@
 from .base import *
+import dj_database_url
 
 DEBUG = False
 
@@ -6,3 +7,11 @@ try:
     from .local import *
 except ImportError:
     pass
+
+DATABASES['default'] =  dj_database_url.config()
+	
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
